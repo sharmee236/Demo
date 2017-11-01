6fbf01baed1ac451c034d87bf843b0836312924c
Sharmin <sharmee236@yahoo.com>
2017-11-01 15:49:59 -0400

    more change

--- DemoForGit/src/demo/DemoClass.java
+++ DemoForGit/src/demo/DemoClass.java
@@ -5,6 +5,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("add this to git practice1 repository");
 		System.out.println("Added Changes");
+		System.out.println("Add more change");
 
 	}
 
