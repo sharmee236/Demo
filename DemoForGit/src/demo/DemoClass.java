3f7d10f100a2f88712147f4a42236bb42ec20549
Sharmin <sharmee236@yahoo.com>
2017-11-01 21:44:48 -0400

    add new

--- DemoForGit/src/demo/DemoClass.java
+++ DemoForGit/src/demo/DemoClass.java
@@ -1,11 +1,17 @@
 package demo;
 
 public class DemoClass {
+	 
 
 	public static void main(String[] args) {
+		 int a=3;
+		 int b=5;
+		 int c= a+b;
 		System.out.println("add this to git practice1 repository");
 		System.out.println("Added Changes");
 		System.out.println("Add more change");
+		System.out.println(c);
+		
 
 	}
 
